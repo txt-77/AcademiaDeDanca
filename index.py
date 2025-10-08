ab3395db3c16e8d079c32d7466aecd49bb035755
txt-77 <gustavomartins717117@gmail.com>
2025-10-08 14:23:18 -0300

    adiçao de bind para as labels

--- index.py
+++ index.py
@@ -1,8 +1,18 @@
 from tkinter import *
 
+def mudar_pagina(pag):
+    match pag:
+        case 1:#inicial
+               return
+        case 2:#sobre nos
+               return
+        case 3:#alugueis
+                return
+    return
 root = Tk()
 root.title("PlaceHolder")
 
+#para emular o formato de um site
 Header= Frame(root)
 Body= Frame(root)
 Footer= Frame(root)
@@ -10,14 +20,27 @@ Header.grid(row=0,column=0,sticky="ew")
 Body.grid(row=1,column=0,sticky="ew")
 Footer.grid(row=2,column=0,sticky="ew")
 
-Label(Header,text="1").grid(row=0,column=0,sticky="w")
-Label(Header,text="",width=20).grid(row=0,column=1,columnspan=2)
-Label(Header,text="2").grid(row=0,column=3)
-Label(Header,text="3").grid(row=0,column=4)
+#informaçoes do topo
+logo = Label(Header,text="logo")
+logo.grid(row=0,column=0,sticky="w")
+logo.bind("<Button-1>", lambda: mudar_pagina(1))
 
+#espaçamento
+Label(Header,text="",width=20).grid(row=0,column=1,columnspan=2)
+
+about = Label(Header,text="sobre nós")
+about.grid(row=0,column=3)
+about.bind("<Button-1>", lambda: mudar_pagina(2))
+
+aula = Label(Header,text="alugar aula")
+aula.grid(row=0,column=4)
+aula.bind("<Button-1>", lambda: mudar_pagina(3))
+
+#corpo (decidir oque colocar aqui)
 Label(Body,text="1").grid(row=0,column=0)
 Label(Body,text="2").grid(row=1,column=0)
 Label(Body,text="3").grid(row=2,column=0)
 
+#informaçoes extras (decidir oque colocar aqui tmb)
 Label(Footer,text="1").grid(row=0,column=0)
 root.mainloop()
