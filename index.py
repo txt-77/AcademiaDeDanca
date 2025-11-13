52f39bd2eb02c35a27e460b5b119be14d02e8f6f
txt-77 <gustavomartins717117@gmail.com>
2025-11-13 13:56:56 -0300

    alteraçao minuscula

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
 from tkinter import *
 
-def mudar_pagina(pag):
-    match pag:
-        case 1:#inicial
-               return
-        case 2:#sobre nos
-               return
-        case 3:#alugueis
-                return
-    return
+# def mudar_pagina(pag):
+#     match pag:
+#         case 1:#inicial
+#                return
+#         case 2:#sobre nos
+#                return
+#         case 3:#alugueis
+#                 return
+#     return
 root = Tk()
 root.title("PlaceHolder")
 
@@ -23,18 +23,18 @@ Footer.grid(row=2,column=0,sticky="ew")
 #informaçoes do topo
 logo = Label(Header,text="logo")
 logo.grid(row=0,column=0,sticky="w")
-logo.bind("<Button-1>", lambda: mudar_pagina(1))
+#logo.bind("<Button-1>", lambda: mudar_pagina(1))
 
 #espaçamento
 Label(Header,text="",width=20).grid(row=0,column=1,columnspan=2)
 
 about = Label(Header,text="sobre nós")
 about.grid(row=0,column=3)
-about.bind("<Button-1>", lambda: mudar_pagina(2))
+#about.bind("<Button-1>", lambda: mudar_pagina(2))
 
 aula = Label(Header,text="alugar aula")
 aula.grid(row=0,column=4)
-aula.bind("<Button-1>", lambda: mudar_pagina(3))
+#aula.bind("<Button-1>", lambda: mudar_pagina(3))
 
 #corpo (decidir oque colocar aqui)
 Label(Body,text="lorem").grid(row=0,column=0)
